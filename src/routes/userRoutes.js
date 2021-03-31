177cdf4f097b5abe84204725b48db5d2b809d44f
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-03-31 15:59:33 -0300

    add middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 router.post('/', UserController.store);
-router.get('/', UserController.index);
-router.get('/:id', UserController.show);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.delete);
+router.get('/', loginRequired, UserController.index);
+router.get('/:id', loginRequired, UserController.show);
+router.put('/:id', loginRequired, UserController.update);
+router.delete('/:id', loginRequired, UserController.delete);
 
 export default router;
