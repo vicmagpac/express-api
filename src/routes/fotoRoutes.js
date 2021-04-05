265fd22ca00b6c76798bd6c1ae6a6d327d74ac95
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-04-05 11:10:12 -0300

    add relationship of aluno foto

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import loginRequired from '../middlewares/loginRequired';
+
 import FotoController from '../controllers/FotoController';
 
 const router = new Router();
 
-router.post('/', FotoController.store);
+router.post('/', loginRequired, FotoController.store);
 
 export default router;
