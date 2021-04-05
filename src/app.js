a850ff97d13afcc2c01711d9589222674b6632a1
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-04-05 12:05:19 -0300

    add scripts sucrase build and start

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import { resolve } from 'path';
 
 dotenv.config();
 
-import './src/database';
+import './database';
 
 import express from 'express';
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import authRoutes from './src/routes/authRoutes';
-import alunoRoutes from './src/routes/alunoRoutes';
-import fotoRoutes from './src/routes/fotoRoutes';
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import fotoRoutes from './routes/fotoRoutes';
 
 class App {
   constructor() {
