b880b8bafe7b41b71d37418bdd592631322947a0
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-04-05 10:27:51 -0300

    add controller foto format

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json('foto');
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import FotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), FotoController.store);
+router.post('/', FotoController.store);
 
 export default router;
