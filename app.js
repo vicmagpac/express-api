a376601a79c98e08e667b6ae0d8a99785253cd16
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-04-05 10:16:37 -0300

    add upload

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import authRoutes from './src/routes/authRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
+import fotoRoutes from './src/routes/fotoRoutes';
 
 class App {
   constructor() {
@@ -29,6 +30,7 @@ class App {
     this.app.use('/users/', userRoutes);
     this.app.use('/auth/', authRoutes);
     this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
   }
 }
 
---
+++ src/routes/fotoRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import FotoController from '../controllers/FotoController';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig);
+
+const router = new Router();
+
+router.post('/', upload.single('foto'), FotoController.store);
+
+export default router;
